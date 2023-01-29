2c99a1ec0ff2ee70ccf078cb8902a83911c478f3
SKBOTZ <123869002+SKBOTZ@users.noreply.github.com>
2023-01-29 22:09:17 +0530

    Add files via upload

--- settings.js
+++ settings.js
@@ -4,10 +4,9 @@
  █░▒█ █░▒█ █▀▀▀ █▀▀▀ █▒█▒█   █▀▀▀ █░░░ ░█░ ▀▀▀▄▄ █▄▄█
  ▀▀█▄ ▀▄▄▀ █▄▄▄ █▄▄▄ █░░▀█   █▄▄▄ █▄▄█ ▄█▄ █▄▄▄█ █░▒█
  
- █▀▀█ ░ █▀▀█ ░ █▀▀█ ░░ █░░▒█
- ░░▀▄ ░ █▄▀█ ░ █▄▀█ ▀▀ ▒█▒█░
- █▄▄█ █ █▄▄█ █ █▄▄█ ░░ ░▀▄▀░
- 
+ █▀▀█ ░ █▀▀█ ░ █▀▀█   ░░   █░░▒█
+ ░░▀▄ ░ ░░▀▄ ░ █▄▀█   ▀▀   ▒█▒█░
+ █▄▄█ █ █▄▄█ █ █▄▄█   ░░   ░▀▄▀░ 
  𝑶𝒘𝒏𝒆𝒓 : 𝑴𝒓 𝑵𝒊𝒎𝒂
  𝑯𝒆𝒍𝒑𝒆𝒓𝒔 : 
  • 𝑷𝒂𝒔𝒊𝒅𝒖
@@ -25,44 +24,44 @@ const chalk = require('chalk')
 
 
 //👇 if you need seen massage use " true "
-global.READ_MASSAGE = 'true'
+global.READ_MASSAGE = 'false'
 //👇 enable desable Girls Voice Reply
 global.VOICE_REPLY =  'true'
 // You Bots Owner Number 
 global.owner = ['94715166712'] 
 //👇 Your Caption ( Image Video )
-global.cap = '© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝚂𝙷𝙰𝙳𝙾𝚆 𝙺𝙸𝙻𝙻𝙴𝚁 𝚆𝙰 𝙱𝙾𝚃²⁰²³'
+global.cap = '© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝙴𝙻𝙸𝚂𝙰 ²⁰²³'
 //👇 Do you want show time on your bio use "on" want desable use "off"
-global.AUTO_BIO =  'off'
+global.AUTO_BIO =  'on'
 //👇 Inbox massage block PM block
-global.INBOX_BLOCK =  'off'
+global.INBOX_BLOCK =  'on'
 //👇 Auto react 
 global.AUTO_REACT =  'true'
 //👇 Bad word Auto delete ( you must add bad words )
 global.ANTI_BADWORD =  'true'
 //👇 Your Bot Name
-global.botnma =  '𝐒𝐇𝐀𝐃𝐎𝐖 𝐊𝐈𝐋𝐋𝐄𝐑 𝐌𝐃 𝐁𝐎𝐓' 
+global.botnma =  '𝐐𝐔𝚵𝚵𝚴 𝚵𝐋𝚰𝐒𝚫 𝚳𝐃 3 𝛁' 
 //👇 Your name
-global.ownernma =  '𝐌𝐑. 𝐃𝐀𝐇𝐀𝐌 𝐏𝐀𝐍𝐒𝐈𝐋𝐔 ' 
+global.ownernma =  '𝑴𝒓 𝑵𝒊𝒎𝒂' 
 //👇 Sticker package name
-global.packname =  '𝐒𝐇𝐀𝐃𝐎𝐖 𝐊𝐈𝐋𝐋𝐄𝐑 𝐌𝐃 𝐁𝐎𝐓' 
+global.packname =  '𝗤𝗨𝗘𝗘𝗡 𝗘𝗟𝗜𝗦𝗔' 
 //👇 Sticker Author Name
-global.author =  '𝚂𝙷𝙰𝙳𝙾𝚆 𝙺𝙸𝙻𝙻𝙴𝚁 𝙾𝙵𝙲' 
+global.author =  '𝙼𝚁 𝙽𝙸𝙼𝙰 𝙾𝙵𝙲' 
 //👇 Kick And Auto Delete Group link Senders
-global.antilink =  'true'
+global.antilink =  'false'
 //👇 Inbox Block Massage 
-global.INBOX_BLOCK_MSG = 'INBOX NOT ALLOWED!!!'
+global.INBOX_BLOCK_MSG = 'BLOCK !!!'
 //👇 Owner React Imoji
 global.OWNER_REACT =  '💻'
 global.BAD_KICK_MSG = '*Bad word detect !*'
 //👇 URL.button 1 name
-global.BUTTON1 = '𝙶𝙸𝚃𝙷𝚄𝙱'
+global.BUTTON1 = '𝙶𝚒𝚝𝚑𝚞𝚋'
 //👇 URL button 2 name
-global.BUTTON2 = '𝚈𝙾𝚄𝚃𝚄𝙱𝙴'
+global.BUTTON2 = '𝚈𝚘𝚞𝚝𝚞𝚋𝚎'
 //👇 Button 1 url 
-global.BUTTON1_URL = 'https://github.com/SKBOTZ/Queen-Elisa-MD-V2/'
+global.BUTTON1_URL = 'https://github.com/darkmakerofc/'
 //👇 Button 2 url
-global.BUTTON2_URL = 'https://youtube.com/@shadowkillermodz'
+global.BUTTON2_URL = 'https://youtube.com/c/MRNIMAOFC'
 //👇 Your Add Massage 
 global.ADD_MSG = '*✅ Success add Group*' 
 //👇 Your Kick Massage 
@@ -78,11 +77,29 @@ global.MENU_IMOJI = '👸|ℹ️'
 //👇 Your time zone ( use correct time zone )
 global.TIME_ZONE = process.env.TIME_ZONE || 'Asia/Colombo'
 //👇 Removebg Api key
-global.REMOVE_BG_API = 'pxVpxefA1K9eGJxEe5kgPzBv'
+global.REMOVE_BG_API = ''
 //👇 Pron Video Download 
-global.SEX_VIDEO_DOWNLOAD = process.env.SEX_VIDEO_DOWNLOAD || 'true'
+global.SEX_VIDEO_DOWNLOAD = process.env.SEX_VIDEO_DOWNLOAD || 'false'
 //👇 Change Bot Language 
 global.LANG = 'EN'
+//Alive logo
+global.alivelogo = process.env.ALIVE_IMAGE || `https://telegra.ph/file/b3f17c16eedf99731a52d.jpg`
+//👇 Alive message
+global.ALIVE_MESSAGE = process.env.ALIVE_MESSAGE || ``
+//👇 Welcome Mesaage
+global.WELCOME = process.env.WELCOME || ``
+//👇 welcome image 
+global.WELCOME_IMAGE = process.env.WELCOME_IMAGE || ``
+//👇 Goodbye message 
+global.GOODBYE = process.env.GOODBYE || ``
+//👇 Good bye image
+global.GOODBYE_IMAGE = process.env.GOODBYE_IMAGE || ``
+//👇 Send Welcome ( true or false )
+global.SEND_WELCOME = process.env.SEND_WELCOME || false
+//👇 Send block command message (true or false )
+global.BLOCK_CMD_MESSAGE_SEND = false
+//👇 Desable PM Block
+global.NO_BLOCK = ["94715166712","94719574492"]
 
 
 
@@ -92,26 +109,34 @@ global.LANG = 'EN'
 
 //MORE
 global.zenzapi = '01ABEB1E11'
-global.tiktokthub = 'https://i.ibb.co/zfP3kV0/IMG-20220217-081502-038.jpg'
+global.tiktokthub = 'https://telegra.ph/file/d83742c9e0a076364184e.jpg'
 //other
-global.pemilik = ['94717069879'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94717069879'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = '𝚂𝙷𝙰𝙳𝙾𝚆 𝙺𝙸𝙻𝙻𝙴𝚁' // Your name ඔබේ නම 
+global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
 global.sessionName = 'session'
-global.prefix = ['#','!','/',''] 
+global.prefix = ['#','!','.'] 
 global.sp = '🔵'
 
 
 global.mess = {
     success: '✅ Done!',
-    admin: '*මෙම විධානය භාවිතා කර හැක්කේ ඇඩ්මින්ලට පමනි !*',
-    botAdmin: '*කරුනාකර බොට් ඇඩ්මින් කරන්න !*',
-    owner: '*කණගාටුයි , මෙම විධානය අයිතිකරුට පමනි !*',
-    group: '*මෙම විධානය සමූහවල පමණක් බාවිතාකරහැක !*',
-    private: '*මෙම විධානය Inbox පමනක් බාවිතාකර හැක !*',
-    bot: '*මෙම විධානය බොට් නම්බර් එකෙන් පමණක් බාවිතාකර හැක !*',
-    wait: '*♲ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ǫᴜᴇᴇɴ ᴇʟɪsᴀ ɪs ᴘʀᴏssᴇsɪɴɢ ...*',
+    admin: '*Only admins can use this command !*',
+    botAdmin: '*Please admin the bot first !*',
+    owner: '*Sorry, this command is only for the owner !*',
+    group: '*This command can only be used in groups !*',
+    private: '*This command can only be used in Inbox !*',
+    bot: '*This command can only be used by bot number !*',
+    wait: '*♲ Please Wait ...*',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    BLOCK_CMD_MSG : '*⚠️ This Command is Block By Owner*',
+    BLOCK : '',
+    UNBLOCK : '',
+    KICK : '',
+    ADD : '',
+    LEAVE : '',
+    PROMOTE : '',
+    DEMOTE : ''
 }
 global.limitawal = {
     premium: "Infinity", 
@@ -139,7 +164,7 @@ global.APIKeys = {
 }
 
 global.thumb = fs.readFileSync('./Media/image/Elisa.jpg')
-global.alivelogo = `https://i.ibb.co/zfP3kV0/IMG-20220217-081502-038.jpg`
+global.alivelogo = `https://telegra.ph/file/b3f17c16eedf99731a52d.jpg`
 global.imgalive = fs.readFileSync('./Media/image/Elisa.jpg')
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
