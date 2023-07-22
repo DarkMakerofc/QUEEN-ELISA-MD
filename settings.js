28b6b6f82c36e135cf91fc184e6a0f7aaa868244
ruwel2007 <138858782+ruwel2007@users.noreply.github.com>
2023-07-22 07:41:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -119,7 +119,7 @@ global.GOODBYE = process.env.GOODBYE || ``
 //👇 Good bye image
 global.GOODBYE_IMAGE = process.env.GOODBYE_IMAGE || ``
 //👇 DISABLE PM Block numbers
-global.NO_BLOCK = ["94715166712","94719574492"]
+global.NO_BLOCK = ['+94753335072']
 //👇 DISABLE download words
 global.IMG_BLOCKER = ["xxx","sex","mia"]
 //👇 Open ai API Put Withouth "sk-" ( "sk-" අයින් කරල ඉතුරු ටික දාන්න )
