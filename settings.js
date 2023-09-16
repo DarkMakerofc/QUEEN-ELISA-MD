1bd2c8092481a815d9001400ad7d76f8178ec629
Aizen090 <130570116+Aizen090@users.noreply.github.com>
2023-09-16 19:50:48 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -81,13 +81,13 @@ global.AUTO_SEEN_STATUS = true
        ✨ BOT INFO SETTINGS ✨
 */
 // You Bots Owner Number 
-global.owner = ['94715166712'] 
+global.owner = ['221778884595'] 
 //👇 Your Caption ( Image Video )
 global.cap = '© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝙴𝙻𝙸𝚂𝙰 ²⁰²³'
 //👇 Your Bot Name
 global.botnma =  '𝐐𝐔𝚵𝚵𝚴 𝚵𝐋𝚰𝐒𝚫 𝚳𝐃 3 𝛁' 
 //👇 Your name
-global.ownernma =  'ᴍ ʀ ⃞ ɴ ɪ ᴍ ᴀ' 
+global.ownernma =  '𝙎𝞢𝙔è 𝞓𝞘𝙕𝞢𝞜༒' 
 //👇 Sticker Author Name
 global.packname =  '𝙼𝚁 𝙽𝙸𝙼𝙰 𝙾𝙵𝙲' 
 //👇 Inbox Block Massage 
@@ -158,11 +158,11 @@ global.mess = {
     BAD_DETECT : '',
     BLOCK : '',
     UNBLOCK : '',
-    KICK : '',
-    ADD : '',
+    KICK : 'Salopard reglel kouleu kick lo ?',
+    ADD : 'Yemal laleu wx',
     LEAVE : '',
-    PROMOTE : '',
-    DEMOTE : ''
+    PROMOTE : 'Tu es une personne responsable te voilà admin'
+    DEMOTE : 'Con que tu es,tu es demis de tes fonctions 
 }
 
 
