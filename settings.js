5894b0b70f364671755fa3cbf100bca4d62cb831
kimjessi <144650599+kimjessi@users.noreply.github.com>
2023-09-22 06:07:05 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -81,15 +81,15 @@ global.AUTO_SEEN_STATUS = true
        ✨ BOT INFO SETTINGS ✨
 */
 // You Bots Owner Number 
-global.owner = ['94715166712'] 
+global.owner = ['237697368307'] 
 //👇 Your Caption ( Image Video )
-global.cap = '© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝙴𝙻𝙸𝚂𝙰 ²⁰²³'
+global.cap = '© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝚸𝚫𝚻𝚪𝚰𝐊 𝚩𝚯𝚻 ²⁰²³'
 //👇 Your Bot Name
-global.botnma =  '𝐐𝐔𝚵𝚵𝚴 𝚵𝐋𝚰𝐒𝚫 𝚳𝐃 3 𝛁' 
+global.botnma =  '𝚸𝚫𝚻𝚪𝚰𝐊 𝚩𝚯𝚻3 𝛁' 
 //👇 Your name
-global.ownernma =  'ᴍ ʀ ⃞ ɴ ɪ ᴍ ᴀ' 
+global.ownernma =  '𝐁𝐥𝟒𝐜𝐊 彡Ⱨ₳₵₭ɆⱤ 彡✞' 
 //👇 Sticker Author Name
-global.packname =  '𝙼𝚁 𝙽𝙸𝙼𝙰 𝙾𝙵𝙲' 
+global.packname =  '𝚸𝚫𝚻𝚪𝚰𝐊 𝚩𝚯𝚻' 
 //👇 Inbox Block Massage 
 global.INBOX_BLOCK_MSG = 'BLOCK !!!'
 //👇 Owner React Imoji
@@ -173,7 +173,7 @@ global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම
 global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
 global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
 global.sessionName = 'session'
-global.prefix = ['#','!','.'] 
+global.prefix = ['*','!',':'] 
 global.sp = '👽'
 global.weem = 'QUEEN ELISA'
 
