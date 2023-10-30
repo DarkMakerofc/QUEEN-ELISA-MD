421b601f81a367dda28b3ab0f6ee9c8acf6a3dbd
Lazaromtaju <148865934+Lazaromtaju@users.noreply.github.com>
2023-10-30 07:21:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -87,7 +87,7 @@ global.cap = '© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴�
 //👇 Your Bot Name
 global.botnma =  '𝐐𝐔𝚵𝚵𝚴 𝚵𝐋𝚰𝐒𝚫 𝚳𝐃 3 𝛁' 
 //👇 Your name
-global.ownernma =  'Lazack' 
+global.ownernma =  'Lazack 28' 
 //👇 Sticker Author Name
 global.packname =  '𝙼𝚁 𝙽𝙸𝙼𝙰 𝙾𝙵𝙲' 
 //👇 Inbox Block Massage 
