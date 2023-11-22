3c37f13474898244fcb796368149c5131064319f
abhimana2006 <149298365+abhibot2006@users.noreply.github.com>
2023-11-23 02:37:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -66,7 +66,7 @@ global.SEX_VIDEO_DOWNLOAD = true
 //👇 Only Inbox Use ( Only PM Work )
 global.INBOX_ONLY_WORK = false
 //👇 Auto ChatGPT ( You Must put CHATGPT API KEY For use this )
-global.AUTO_OPEN_AI = true
+global.AUTO_OPEN_AI = false
 //👇 Bot On Privet
 global.PRIVET_BOT = false
 //👇 DISABLE Bot Commands But Others Are Working 
